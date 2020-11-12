4cb6bc17104766bded2fc45723849ed1ee0983b1
Victor Oliveira <oliveira.vitoco@gmail.com>
2020-11-12 11:08:20 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var display = document.getElementById("output");
+var mini = document.getElementById("first-number");
+var maxi = document.getElementById("second-number");
+
+function tela(value){
+    display.value = value;
+}
+function limpartela(){
+    mini.value = ''
+    maxi.value = ''
+	display.value = ''
+}
+
+
+
+
+function rand(){
+    var min = Number(mini.value)
+    var max = Number(maxi.value)
+    var result = Math.random()* (max - min) + min;
+    tela(Math.round(result))
+}
